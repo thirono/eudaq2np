d02efe2e08e331cf109a67c9c075e935c46f264c
themperek <themperek@gmail.com>
2014-11-21 19:46:38 +0100

    Fix selection by type

--- eudaq2np/lib/cfunc.cxx
+++ eudaq2np/lib/cfunc.cxx
@@ -21,10 +21,18 @@ void eudaq_data_vector(const std::string & filename, const std::string & type, s
 // Create a reader for this iframele
   eudaq::FileReader reader(filename);
 
+  eudaq::PluginManager::Initialize(reader.GetDetectorEvent());
+
   // Display the actual iframelename (argument could have been a run number)
   //std::cout << "Opened file: " << reader.Filename() << std::endl;
 
   while (reader.NextEvent()) {
+       if (reader.GetDetectorEvent().IsEORE()) {
+          std::cout << "End of run detected" << std::endl;
+          // Don't try to process if it is an EORE
+          break;
+        }
+
       try {
         //const eudaq::RawDataEvent & rev = reader.GetDetectorEvent().GetRawSubEvent(type);
         //std::cout << rev << std::endl;
@@ -35,23 +43,27 @@ void eudaq_data_vector(const std::string & filename, const std::string & type, s
         for(unsigned int iplane = 0; iplane < sev.NumPlanes(); iplane++){
           const StandardPlane & plane = sev.GetPlane(iplane);
             
-            for(unsigned int iframe = 0; iframe < plane.NumFrames(); iframe++){
-                   
-              const std::vector<double> & xv = plane.XVector(iframe);
-              const std::vector<double> & yv = plane.YVector(iframe);
-              const std::vector<double> & pix = plane.PixVector(iframe);
-              
-              for(int ipix = 0; ipix < pix.size(); ipix++){
-                    //std::cout << plane.TLUEvent() << " " << iplane << " "<< iframe << " " << xv[ipix] << " " << yv[ipix] << " "<< pix[ipix] << "" << std::endl;
-                    data_row data_pix = {plane.TLUEvent(), iplane, iframe, plane.ID(), xv[ipix],  yv[ipix], pix[ipix]};
-                    data.push_back(data_pix);
-              }                     
-            }
+	    //std::cout<< " sensor=" << plane.Sensor() << " type=" << plane.Type() << "" << std::endl;
+	    if(plane.Type() == type){
+	      for(unsigned int iframe = 0; iframe < plane.NumFrames(); iframe++){
+		    
+		const std::vector<double> & xv = plane.XVector(iframe);
+		const std::vector<double> & yv = plane.YVector(iframe);
+		const std::vector<double> & pix = plane.PixVector(iframe);
+		
+		for(unsigned int ipix = 0; ipix < pix.size(); ipix++){
+		      //std::cout << plane.TLUEvent() << " " << iplane << " "<< iframe << " " << xv[ipix] << " " << yv[ipix] << " "<< pix[ipix] << "" << std::endl;
+		      data_row data_pix = {plane.TLUEvent(), iplane, iframe, plane.ID(), xv[ipix],  yv[ipix], pix[ipix]};
+		      data.push_back(data_pix);
+		}                  
+	      }
+	    }
 
         }
+        
       } catch (const eudaq::Exception & e) {
-        //std::cout << "No " << op.GetArg(0) << " subevent in event "  << reader.GetDetectorEvent().GetEventNumber() << std::endl;
-      }    
+        //std::cout << "No " << type << " subevent in event "  << reader.GetDetectorEvent().GetEventNumber() << std::endl;
+      } 
 
     }
 }

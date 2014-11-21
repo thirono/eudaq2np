d273e1415d529adf2a4f8926aa57d485c7899bb9
Tomasz Hemperek <themperek@users.noreply.github.com>
2014-11-21 16:03:45 +0100

    should be faster now, still one copy ;-(

--- eudaq2np/wrapped.pyx
+++ eudaq2np/wrapped.pyx
@@ -1,25 +1,29 @@
 cimport cython
 import numpy as np
-cimport numpy as cnp
+cimport numpy as np
 
 from libcpp.string cimport string as std_string
 from libcpp.vector cimport vector as std_vector
 
-cimport numpy as cnp
-cnp.import_array()  # if array is used it has to be imported, otherwise possible runtime error
+np.import_array()  # if array is used it has to be imported, otherwise possible runtime error
+
+from libc.stdlib cimport malloc
+ctypedef np.int8_t DTYPE_t
 
 cdef extern from "lib/cfunc.h":
 
     void eudaq_data_vector(const std_string & filename, const std_string & type, std_vector[data_row] & data)
 
     struct data_row:
-        cnp.uint32_t tluevent
-        cnp.uint32_t plane
-        cnp.uint32_t frame
-        cnp.uint32_t id
-        cnp.float64_t x
-        cnp.float64_t y
-        cnp.float64_t val
+        np.uint32_t tluevent
+        np.uint32_t plane
+        np.uint32_t frame
+        np.uint32_t id
+        np.float64_t x
+        np.float64_t y
+        np.float64_t val
+
+cdef dt = np.dtype([('tluevent', '<u4'), ('plane', '<u4'), ('frame', '<u4'), ('id', '<u4'), ('x', '<f8'), ('y', '<f8'), ('val', '<f8')]) 
 
 def data_np(filename, type):
 
@@ -27,19 +31,14 @@ def data_np(filename, type):
 
     eudaq_data_vector(<const std_string &> filename ,<const std_string &> type, <std_vector[data_row] &> vect)
 
-    dt = np.dtype([('tluevent', '<u4'), ('plane', '<u4'), ('frame', '<u4'), ('id', '<u4'), ('x', '<f8'), ('y', '<f8'), ('val', '<f8')]) 
+    arr = data_to_numpy_array_with_spec(&vect[0], vect.size() * sizeof(data_row), np.NPY_INT8)
 
-    out = np.empty([vect.size()], dtype=dt)
-    
-    # this is not smart and slow
-    for i in range(vect.size()):
-        out['x'][i] = vect[i].x
-        out['y'][i] = vect[i].y
-        out['val'][i] = vect[i].val
+    return arr.copy().view(dt) # should be way to do it without copy
 
-        out['tluevent'][i] = vect[i].tluevent
-        out['plane'][i] = vect[i].plane
-        out['frame'][i] = vect[i].frame
-        out['id'][i] = vect[i].id
+#cdef extern from "numpy/arrayobject.h":
+#    void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
 
-    return out
+cdef data_to_numpy_array_with_spec(void * ptr, np.npy_intp N, int t):
+    cdef np.ndarray[DTYPE_t, ndim=1] arr = np.PyArray_SimpleNewFromData(1, &N, t, ptr)
+    #PyArray_ENABLEFLAGS(arr, np.NPY_OWNDATA)
+    return arr
